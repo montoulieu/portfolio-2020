f7d4edcfd0f13ec5b98ab4e5f9c46c6c9f3e1fff
Pieter Montoulieu <pieter@montoulieu.dev>
2020-09-07 23:58:52 -0600

    Postcss updates

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,22 @@
 module.exports = {
-  plugins: ['tailwindcss', 'postcss-preset-env'],
-}
+  plugins: [
+    'postcss-import',
+    'tailwindcss',
+    'postcss-nested',
+    process.env.NODE_ENV === 'production'
+      ? [
+        '@fullhuman/postcss-purgecss',
+        {
+          content: [
+            './pages/**/*.{js,jsx,ts,tsx}',
+            './components/**/*.{js,jsx,ts,tsx}',
+          ],
+          whitelist: ['pagination', 'active', 'disabled', 'xl:block'],
+          whitelistPatterns: [/^slick/, /^ril/, /^wp/, /^bg-/, /^w-/, /^p-/, /^xl:w-/],
+          defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+        },
+      ]
+      : undefined,
+    'autoprefixer',
+  ],
+};
