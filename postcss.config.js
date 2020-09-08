cdfa986d43bd5fe9cd25fe6949fb8ffa252755de
Pieter Montoulieu <pieter@montoulieu.dev>
2020-09-08 16:59:08 -0600

    Tailwind and postcss config updates

--- postcss.config.js
+++ postcss.config.js
@@ -3,20 +3,6 @@ module.exports = {
     'postcss-import',
     'tailwindcss',
     'postcss-nested',
-    process.env.NODE_ENV === 'production'
-      ? [
-        '@fullhuman/postcss-purgecss',
-        {
-          content: [
-            './pages/**/*.{js,jsx,ts,tsx}',
-            './components/**/*.{js,jsx,ts,tsx}',
-          ],
-          whitelist: [],
-          whitelistPatterns: [],
-          defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-        },
-      ]
-      : undefined,
     'autoprefixer',
   ],
 };
